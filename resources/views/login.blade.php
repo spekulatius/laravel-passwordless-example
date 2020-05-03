1dfdcd06428e49e158a3181e8c1648504df4cc5d
Peter Thaleikis <peter.thaleikis@gmail.com>
2020-05-03 10:13:17 +0400

    Adding login form and route

---
+++ resources/views/login.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://andybrewer.github.io/mvp/mvp.css">
+    <title>Login</title>
+  </head>
+  <body>
+    <main>
+      <section>
+        <form>
+          <header>
+            <h2>Login</h2>
+          </header>
+          <label for="input1">Email Address:</label>
+          <input name="email" type="email" size="20" placeholder="Your email address">
+          <button type="submit">Login</button>
+        </form>
+      </section>
+    </main>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () { return view('login'); });
