4113da2d66ec2e9323552b8e279ee87bee921e14
Peter Thaleikis <peter.thaleikis@gmail.com>
2020-05-03 10:29:38 +0400

    Adding draft for the AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * Processes the login form
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return redirect
+     **/
+    public function login(Request $request)
+    {
+        // Coming soon
+    }
+
+    /**
+     * Processes the actual login
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return redirect
+     **/
+    public function signIn(Request $request)
+    {
+        // Coming soon
+    }
+
+    /**
+     * Processes the logout
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return redirect
+     **/
+    public function logout(Request $request)
+    {
+        // Coming soon
+    }
+}
